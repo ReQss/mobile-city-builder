4f35d9c91d5ac40a5054145dea7a4ffa83d656a1
Łukasz Ledzion <reqspl@gmail.com>
2025-07-10 15:20:27 +0200

    options

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +7,8 @@ using UnityEngine.UI;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
+    [SerializeField]
+    private List<Sprite> canvasBackgrounds;
     [SerializeField]
     private GameObject loaderCanvas;
     [SerializeField]
@@ -23,24 +26,48 @@ public class LevelManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    public Sprite RandomSpriteGenerate()
+    {
+        if (canvasBackgrounds != null && canvasBackgrounds.Count > 0)
+        {
+            int randomIndex = Random.Range(0, canvasBackgrounds.Count);
+            return canvasBackgrounds[randomIndex];
+        }
+        return null;
+    }
     public async void LoadScene(string sceneName)
     {
+        if (sceneName == "Maps" || sceneName == "Menu")
+        {
+            LoadSceneNoLoader(sceneName);
+            return;
+        }
         target = 0f;
         progressBar.fillAmount = 0f;
         var scene = SceneManager.LoadSceneAsync(sceneName);
         scene.allowSceneActivation = false;
+        Sprite randomSprite = RandomSpriteGenerate();
+        if(randomSprite!=null)
+            loaderCanvas.GetComponent<Image>().sprite = randomSprite;
         loaderCanvas.SetActive(true);
         do
         {
             await Task.Delay(100);
             target = scene.progress;
         } while (scene.progress < 0.9f);
-          await Task.Delay(1000);
+        await Task.Delay(1000);
         scene.allowSceneActivation = true;
-        loaderCanvas.SetActive(false);
 
+        await Task.Yield();
+        loaderCanvas.SetActive(false);
     }
-    // Update is called once per frame
+    public async void LoadSceneNoLoader(string sceneName)
+    { 
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
     void Update()
     {
         progressBar.fillAmount = Mathf.MoveTowards(progressBar.fillAmount, target, 3 * Time.deltaTime);
